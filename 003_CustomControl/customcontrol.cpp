01a9fdcff9b86827ce46633351719cc5763d4a79
张建伟 <dream_dog@163.com>
2021-11-05 21:12:44 +0800

    增加属性测试代码
    增加属性测试代码

--- 003_CustomControl/customcontrol.cpp
+++ 003_CustomControl/customcontrol.cpp
@@ -3,4 +3,20 @@
 CustomControl::CustomControl(QWidget *parent) :
     QWidget(parent)
 {
+    m_label = new QLabel(this);
+    m_label->setGeometry(0,0,100,20);
+}
+
+int CustomControl::getTestID()
+{
+    return testID;
+}
+
+void CustomControl::setTestID(int temp)
+{
+    testID = temp;
+    m_label->setGeometry(0,0,100,20);
+    m_label->setStyleSheet("color: rgb(255, 255, 0);font: 11pt '黑体';");
+    m_label->setText(QString::number(testID,10));
+    update();
 }
--- 003_CustomControl/customcontrol.h
+++ 003_CustomControl/customcontrol.h
@@ -2,13 +2,25 @@
 #define CUSTOMCONTROL_H
 
 #include <QWidget>
-
+#include <QLabel>
 class CustomControl : public QWidget
 {
     Q_OBJECT
 
+    Q_PROPERTY(int testID READ getTestID WRITE setTestID)
+
+
 public:
     CustomControl(QWidget *parent = 0);
+
+    int testID = 1;
+    int getTestID();
+    void setTestID(int temp);
+
+private:
+    QLabel *m_label = nullptr;
+
+
 };
 
 #endif // CUSTOMCONTROL_H
