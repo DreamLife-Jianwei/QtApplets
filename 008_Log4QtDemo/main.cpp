c78e0ba7c9e9db4375b72329b6098ecd6e3bfe9c
张建伟 <dream_dog@163.com>
2022-01-17 16:19:31 +0800

    增加辅助文件

--- 008_Log4QtDemo/main.cpp
+++ 008_Log4QtDemo/main.cpp
@@ -5,16 +5,16 @@
 #include "log4qt/logger.h"
 #include "log4qt/basicconfigurator.h"
 #include "log4qt/propertyconfigurator.h"
+
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
 
-    Log4Qt::PropertyConfigurator::configure(a.applicationDirPath()
-        + "/log4qt.conf");
 
-    Log4Qt::Logger *a1 = Log4Qt::Logger::logger("A1");
-        a1->debug("the message of log");
+
 
     Widget w;
     w.show();
